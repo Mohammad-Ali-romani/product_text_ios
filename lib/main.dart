69566710842ff51cd8b62e1b4c2e6e7e159bcef7
mohammad ali <mohammad.ali.romani@gmail.com>
2023-10-17 10:12:19 +0300

    add map

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(const App());
+void main() => runApp(const MyApp());
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State createState() => _MyAppState();
+}
+
+class _MyAppState extends State {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text("App")),
-            body: Container(
-              // foregroundDecoration: BoxDecoration(border: ),
-              width: MediaQuery.of(context).size!.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text("this is app"),
-                  Icon(Icons.av_timer_sharp),
-                  Image.asset("images/image.jpg")
-                ],
-              ),
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
